9fd57dc21c50067a9d8a6fcaf2f636496d771349
david-bruewer <60238189+davidbruewer@users.noreply.github.com>
2020-08-11 11:49:27 -0400

    Add Folders

--- Assets/Scripts/Multplayer/Player/MPScoreKeeping.cs
+++ Assets/Scripts/Multplayer/Player/MPScoreKeeping.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+//Keeps track of the players score while connected 
 public class MPScoreKeeping : NetworkBehaviour
 {
+    //Player Game object 
     public GameObject player; 
+
+    //Boolean which checks if the player is disconnected 
     bool disconnected; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +20,8 @@ public class MPScoreKeeping : NetworkBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //If Player is connected and their hunger reaches 0, disconnect 
         if (disconnected) {return;}
         if (player.GetComponent<Hunger>().hunger <= 0)
         {
--- Assets/Scripts/Multplayer/Player/MPShooting.cs
+++ Assets/Scripts/Multplayer/Player/MPShooting.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+//Class which defines shooting while multiplayer 
 public class MPShooting : NetworkBehaviour
 {
-  //Public objects
+    //Point which arrow shoots from 
     [SyncVar]
     public Transform firePoint;
+
+    //Arrow prefab 
     public GameObject arrowPrefab;
 
+    //Force and speed of arrow when fired 
     public float arrowForce = 30f;
 
+    //Boolean if arrow can be fired 
     private bool allowFire = true;
 
+    //Rate at which arrow can be shot 
     public float fireRate = 0.75f;
+
     // Update is called once per frame
-    
     void Update()
     {
+        //Checks if mouse has been clicked and firing is allowed 
         if(Input.GetButtonDown("Fire1") && allowFire)
         {
+            //Tells the server to shoot arrow 
             CmdShoot();
             StartCoroutine(ShootTimer());
         }
         
     }
 
+    //Command which tells arrow to instantiate and spawn arrow 
     [Command]
     void CmdShoot()
     {
@@ -37,6 +46,7 @@ public class MPShooting : NetworkBehaviour
         NetworkServer.Spawn(arrow);
     }
 
+    //Time which only allows shooting after a delay of time 
     IEnumerator ShootTimer()
     {
 
